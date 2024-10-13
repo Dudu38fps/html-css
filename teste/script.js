b844cf4ed60c0542fd3d29484575a4a6cb55971b
Eduardo Feliciano <138251065+Dudu38fps@users.noreply.github.com>
2024-10-13 20:56:44 -0300

    teste parte 2 concluída

--- teste/script.js
+++ teste/script.js
@@ -2,10 +2,20 @@
 function clicar() {
 var azul = document.getElementById('azul')
 var vermelho = document.getElementById('vermelho')
+var roxo = document.getElementById('roxo')
 var imagem = document.getElementById('imagem')
+var container = document.getElementById('container')
 
 azul.style.marginLeft = vermelho.offsetLeft + 'px';
 azul.style.marginTop = (vermelho.offsetTop + vermelho.offsetHeight) + 'px';
+
+azul.style.display = 'none'
+vermelho.style.display = 'none'
+roxo.style.alignSelf = 'center'
+roxo.style.backgroundColor = 'purple'
+container.style.flexDirection = 'column'
+container.style.alignItems = 'center'
+
 
 imagem.style.display = 'block';
 
